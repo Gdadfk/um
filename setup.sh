c89b4d36f86ad485c05f1d15342042b809f0a269
Gdadfk <76163453+Gdadfk@users.noreply.github.com>
2023-09-11 00:13:20 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+chmod 777 start.sh
+chmod 777 um
+cd /data/data/com.termux/files/home/storage/downloads
+mkdir um
+cd um
+mkdir input
+mkdir output
+rm /data/data/com.termux/files/home/setup.sh
---
+++ start.sh
@@ -0,0 +1,1 @@
+./um -o /data/data/com.termux/files/home/storage/downloads/um/output --update-metadata -i /data/data/com.termux/files/home/storage/downloads/um/input
